954ba7618af113fbab5cf35a773b66937d6a3a53
SmelJey <mixalevuriy@gmail.com>
2021-04-24 20:32:14 +1000

    Add cave generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -16,24 +16,34 @@ public class LevelGenerator {
         Floor
     }
 
+    private const int StartRadius = 3;
+    
     private const int MapWidth = 100;
     private const int MapChunkLength = 100;
     private const int ChunkPreGeneration = 3;
 
     private const int MaxExitPerChunk = 4;
     private const int MaxExitWidth = 5;
+
+    private const int BorderSize = 4;
     
     private List<TileType[]> levelMap;
 
     private int myLastChunk = 0;
 
     public void InitLevel(GameObject levelHost) {
-        myRandom = new Random(1);
+        myRandom = new Random(2);
         levelMap = new List<TileType[]>();
         GenerateLevel(levelHost, ChunkPreGeneration);
     }
 
     public void GenerateLevel(GameObject levelHost, int chunkCount) {
+        for (int i = -BorderSize; i < 0; i++) {
+            for (int j = -BorderSize; j < MapWidth + BorderSize; j++) {
+                Object.Instantiate(myPreset.WallPrefab[0], IdxToWorldPos(i, j), Quaternion.identity, levelHost.transform);
+            }
+        }
+        
         for (int i = 0; i < chunkCount; i++) {
             GenerateChunk(levelHost);
         }
@@ -74,6 +84,11 @@ public class LevelGenerator {
         for (int round = 0; round < myPreset.AutomatonRounds; round++) {
             for (int i = curY; i < endY; i++) {
                 for (int j = 0; j < MapWidth; j++) {
+                    if (i <= StartRadius * 2 && j >= MapWidth / 2 - StartRadius && j <= MapWidth / 2 + StartRadius) {
+                        bufferMap[i - curY][j] = TileType.Floor;
+                        continue;
+                    }
+                    
                     int wallCnt = GetNeighboursWallCount(j, i);
                     if (levelMap[i][j] == TileType.Floor) {
                         bufferMap[i - curY][j] = (wallCnt >= myPreset.WallCntToWall ? TileType.Wall : TileType.Floor);
@@ -91,8 +106,8 @@ public class LevelGenerator {
         }
 
         for (int i = curY; i < endY; i++) {
-            for (int j = 0; j < MapWidth; j++) {
-                if (levelMap[i][j] == TileType.Wall) {
+            for (int j = 0 - BorderSize; j < MapWidth + BorderSize; j++) {
+                if (j < 0 || j >= MapWidth || levelMap[i][j] == TileType.Wall) {
                     Object.Instantiate(myPreset.WallPrefab[0], IdxToWorldPos(i, j), Quaternion.identity, levelHost.transform);
                 }
             }
@@ -109,22 +124,17 @@ public class LevelGenerator {
     private int GetNeighboursWallCount(int xj, int yi) {
         int cnt = 0;
         for (int i = -1; i <= 1; i++) {
-            if (yi + i < 0 || yi + i >= levelMap.Count) {
-                cnt++;
-                continue;
-            }
-
             for (int j = -1; j <= 1; j++) {
                 if (i == 0 && j == 0) {
                     continue;
                 }
 
-                if (xj + j < 0 || xj + j >= MapWidth) {
+                if (xj + j < 0 || xj + j >= MapWidth || yi + i < 0 || yi + i >= levelMap.Count) {
                     cnt++;
                     continue;
                 }
 
-                if (levelMap[yi][xj] == TileType.Wall) {
+                if (levelMap[yi + i][xj + j] == TileType.Wall) {
                     cnt++;
                 }
             }
